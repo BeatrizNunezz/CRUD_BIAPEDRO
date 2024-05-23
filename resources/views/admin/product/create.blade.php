aae2ef0fba85bbf8f7108c8eda7176530e3517b7
BeatrizNunezz <annabiafer30@gmail.com>
2024-05-23 15:56:53 -0300

    first commit

---
+++ resources/views/admin/product/create.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sn sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                <p><a href="{{ route('admin/products') }}" class="btn btn-primary">Go Back</a></p>
+
+                <form action="" method="POST" enctype="multipart/form-data">
+                    @csrf 
+                    <div class="row mb-3"> 
+                        <div> class="col">
+                            <input type="text" name="title" class="form-control" placeholder="Title">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" name="category" class="form-control" placeholder="Catefory">
---
+++ resources/views/admin/product/home.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Product') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="d-flex align-iten-center justify-content-between">
+                    <h1 class="mb-0">List Product<h1>
+                        <a href="{{ route('admin/products/creat') }}" class="btn btn-primary">Add Product</a>
+                    </div>
+                    Table Content
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
